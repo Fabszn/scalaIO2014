d7a820dc946463d67bbc988de65c6d82e4bbd5ba
Fabrice Sznajderman <fabszn@gmail.com>
2014-10-22 21:09:46 +0200

    fix

--- src/OthersSample.scala
+++ src/OthersSample.scala
@@ -1,14 +1,12 @@
-/**
- * Created by fabricesnajdermann on 18/10/14.
- */
+
 object OthersSample extends App{
 
 
-  val l = List(1, 3, 4, 5, 6, 8, 9)
+  val l = List("apple", "pear", "orange", "banana")
 
-  println(l.lift(1).map(i => i+10).getOrElse("no value found"))
-  println(l.lift(5).map(i => i+10).getOrElse("no value found"))
-  println(l.lift(7).map(i => i+10).getOrElse("no value found"))
+  println(l.lift(1))
+  println(l.lift(3))
+  println(l.lift(7))
 
 
 }
--- src/Sample.scala
+++ src/Sample.scala
@@ -1,12 +1,9 @@
-/**
- * Created by fsznajderman on 20/10/2014.
- */
-object Sample1 extends App {
+
+object Sample extends App {
 
   case class Person(name: String)
 
   val p1: Person = null; // no value
-
 
   val p2: Person = Person("Harikovair") // instance of Person
 
